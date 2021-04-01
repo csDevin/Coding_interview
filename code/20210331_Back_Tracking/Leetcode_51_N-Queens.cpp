5983a71af804106c7262a3d519c61640c8aced76
csDevin <1561472800@qq.com>
2021-04-01 19:58:13 +0800

    L51&79

--- code/20210331_Back_Tracking/Leetcode_51_N-Queens.cpp
+++ code/20210331_Back_Tracking/Leetcode_51_N-Queens.cpp
@@ -1,3 +1,61 @@
 // L51: N-Queens (Hard)
 // 时间复杂度：
 // 空间复杂度：
+
+
+// 回溯法
+// 关键问题：如何判断第n行的第i个位置是否与上面几行的任何一个皇后冲突，列冲突还好判断（使用一个01数组记录），对角线冲突很难判断。
+// 使用三个集合进行判断
+// auto columns = unordered_set<int>();  // 判断当前列是否使用，范围为[0,n-1]
+// auto diagonals1 = unordered_set<int>();  // 判断与左对角线平行的斜线，同一左斜线的差值为唯一定值，从左到右的范围是n-1到 -(n-1);
+// auto diagonals2 = unordered_set<int>();  // 判断与右对角线平行的斜线，同一右斜线的和为唯一定值，从左到右的范围是0到2(n-1);
+
+
+void dfs(int n, int line, int x, unordered_set<int>& col, unordered_set<int>& diag1, unordered_set<int>& diag2, vector<string>& temp, vector<vector<string>>& result)
+{
+    if(col.count(x)==1 || diag1.count(line-x)==1 || diag2.count(line+x)==1)
+        return;
+    temp[line][x] = 'Q';
+    if(line == n-1)
+    {
+        result.push_back(temp);
+    }
+    col.insert(x);
+    diag1.insert(line-x);
+    diag2.insert(line+x);
+    for(int i=0; i<n; i++)
+    {
+        dfs(n, line+1, i, col, diag1, diag2, temp, result);
+    }
+    temp[line][x] = '.';
+    col.erase(x);
+    diag1.erase(line-x);
+    diag2.erase(line+x);
+    return;
+}
+
+vector<vector<string>> solveNQueens(int n) {
+    unordered_set<int> col, diag1, diag2;
+    vector<string> temp(n, string(n, '.'));
+    vector<vector<string>> result;
+    for(int i=0; i<n; i++)
+    {
+        dfs(n, 0, i, col, diag1, diag2, temp, result);
+    }
+    return result;
+}
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    unordered_set<int> col, diag2;
+    vector<int> diag1;
+    vector<string> s(3, string(10, 'c'));
+    // s.insert(0, 10, 'c');
+    // cout << s;
+
+    int aaa = 0;
+}
--- code/20210331_Back_Tracking/Leetcode_79_Word Search.cpp
+++ code/20210331_Back_Tracking/Leetcode_79_Word Search.cpp
@@ -2,50 +2,51 @@
 // 时间复杂度：
 // 空间复杂度：
 
-
-#include<bits/stdc++.h>
+// 自己的代码：先判断，符合条件再dfs
+#include <bits/stdc++.h>
 using namespace std;
 
-vector<int> dir={-1, 0, 1, 0, -1};
-void dfs(vector<vector<char>>& board, string word, vector<vector<int>>& visited, bool& result, int x, int y, int depth)
+vector<int> dir = {-1, 0, 1, 0, -1};
+void dfs(vector<vector<char>> &board, string word, vector<vector<int>> &visited, bool &result, int x, int y, int depth)
 {
-    if(depth==word.size()-1)  //***word.size()-1写成了word.size()
+    if (depth == word.size() - 1) //***word.size()-1写成了word.size()
     {
         result = true;
         return;
     }
-    for(int i=0; i<4; i++)
+    for (int i = 0; i < 4; i++)
     {
-        int x_new = x+dir[i];
-        int y_new = y+dir[i+1];
-        if(x_new<0 || x_new>=board.size() || y_new<0 || y_new>=board[0].size())
-        //***x_new>=board.size()写成了x_new>board.size()
+        int x_new = x + dir[i];
+        int y_new = y + dir[i + 1];
+        if (x_new < 0 || x_new >= board.size() || y_new < 0 || y_new >= board[0].size())
+            //***x_new>=board.size()写成了x_new>board.size()
             continue;
-        if(visited[x_new][y_new]==0 && board[x_new][y_new] == word[depth+1])
+        if (visited[x_new][y_new] == 0 && board[x_new][y_new] == word[depth + 1])
         //***visited[x_new][y_new]==0写成了visited[x_new][y_new]=0;
         {
-            visited[x_new][y_new]=1;
-            dfs(board, word, visited, result, x_new, y_new, depth+1);
-            visited[x_new][y_new]=0;
+            visited[x_new][y_new] = 1;
+            dfs(board, word, visited, result, x_new, y_new, depth + 1);
+            visited[x_new][y_new] = 0;
         }
     }
     return;
 }
-bool exist(vector<vector<char>>& board, string word) {
+bool exist(vector<vector<char>> &board, string word)
+{
     int xLen = board.size();
     int yLen = board[0].size();
-    bool result=false;
+    bool result = false;
     vector<vector<int>> visited(xLen, vector<int>(yLen));
-    for(int i=0; i<xLen; i++)
+    for (int i = 0; i < xLen; i++)
     {
-        for(int j=0; j<yLen; j++)
+        for (int j = 0; j < yLen; j++)
         {
-            if(board[i][j] == word[0])
+            if (board[i][j] == word[0])
             {
-                visited[i][j]=1;
+                visited[i][j] = 1;
                 dfs(board, word, visited, result, i, j, 0);
-                visited[i][j]=0;
-                if(result==true)
+                visited[i][j] = 0;
+                if (result == true)
                     return true;
             }
         }
@@ -53,12 +54,65 @@ bool exist(vector<vector<char>>& board, string word) {
     return false;
 }
 
+
+
+
+
+
+
+// another code：先进dfs，进去了再在开头判断是否符合条件，使用多条if
+// 主函数
+bool exist(vector<vector<char>> &board, string word)
+{
+    if (board.empty())
+        return false;
+    int m = board.size(), n = board[0].size();
+    vector<vector<bool>> visited(m, vector<bool>(n, false));
+    bool find = false;
+    for (int i = 0; i < m; ++i)
+    {
+        for (int j = 0; j < n; ++j)
+        {
+            backtracking(i, j, board, word, find, visited, 0);
+        }
+    }
+    return find;
+}
+// 辅函数
+void backtracking(int i, int j, vector<vector<char>> &board, string &word, bool &find, vector<vector<bool>> &visited, int pos)
+{
+    if (i < 0 || i >= board.size() || j < 0 || j >= board[0].size())  // 越界放第一个判断
+    {
+        return;
+    }
+    if (visited[i][j] || board[i][j] != word[pos])
+    {
+        return;
+    }
+    if (pos == word.size() - 1)
+    {
+        find = true;
+        return;
+    }
+    visited[i][j] = true; // 修改当前节点状态
+    // 递归子节点
+    backtracking(i + 1, j, board, word, find, visited, pos + 1);
+    backtracking(i - 1, j, board, word, find, visited, pos + 1);
+    backtracking(i, j + 1, board, word, find, visited, pos + 1);
+    backtracking(i, j - 1, board, word, find, visited, pos + 1);
+    visited[i][j] = false; // 回改当前节点状态
+}
+
+
+
+
+
 int main()
 {
     vector<vector<char>> board =
         {{'A', 'B', 'C', 'E'},
-        {'S', 'F', 'C', 'S'},
-        {'A', 'D', 'E', 'E'}};
+         {'S', 'F', 'C', 'S'},
+         {'A', 'D', 'E', 'E'}};
     string word = "ABCCED";
     exist(board, word);
     int a = 2;
