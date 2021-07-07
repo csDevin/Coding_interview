2d3d368841a5c5cd0c1d5d841beef26ec01fff0b
csDevin <1561472800@qq.com>
2021-07-07 19:58:51 +0800

    florida

--- leetcode_hot_100/leet141_linklist_cycle.cpp
+++ leetcode_hot_100/leet141_linklist_cycle.cpp
@@ -1,6 +1,15 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+// 方法1：哈希表
+// 方法2：快慢指针
+
+/*
+观察下面的代码，我们使用的是 while 循环，循环条件先于循环体。由于循环条件一定是判断快慢指针是否重合，
+如果我们将两个指针初始都置于 head，那么 while 循环就不会执行。因此，我们可以假想一个在 head 之前的虚拟节点，
+慢指针从虚拟节点移动一步到达 head，快指针从虚拟节点移动两步到达 head.next，这样我们就可以使用 while 循环了。
+当然，我们也可以使用 do-while 循环。此时，我们就可以把快慢指针的初始值都置为 head。
+*/
 
 // Definition for singly-linked list.
 struct ListNode {
