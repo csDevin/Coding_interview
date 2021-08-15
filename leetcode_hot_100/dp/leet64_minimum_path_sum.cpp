79084c59f9e4b08f71eebfd293f8d4381225532b
csDevin <1561472800@qq.com>
2021-08-15 21:08:49 +0800

    c

---
+++ leetcode_hot_100/dp/leet64_minimum_path_sum.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+// 网格数组中的最小路径和
+// dp[i][j]: 从起点走到i,j的最小路径和。
+// i==0: dp[0][j] = dp[0][j-1] + grid[0][j]
+// j==0: dp[i][0] = dp[i-1][0] + grid[i][0]
+// others: dp[i][j] = min(dp[i-1][j], dp[i][j-1]) + grid[i][j]
+class Solution {
+public:
+    int minPathSum(vector<vector<int>>& grid) {
+        if(grid.size()==0 || grid[0].size()==0)
+            return 0;
+        int rows=grid.size(), columns=grid[0].size();
+        vector<vector<int>> dp(rows, vector<int>(columns));
+        dp[0][0] = grid[0][0];
+        for(int i=1; i<rows; i++){
+            dp[i][0] = dp[i-1][0] + grid[i][0];
+        }
+        for(int j=1; j<columns; j++){
+            dp[0][j] = dp[0][j-1] + grid[0][j];
+        }
+        for(int i=1; i<rows; i++){
+            for(int j=1; j<columns; j++){
+               dp[i][j] = min(dp[i-1][j], dp[i][j-1]) + grid[i][j];
+            }
+        }
+        return dp[rows-1][columns-1];
+    }
+};
